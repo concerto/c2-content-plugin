20f126a18ad3593b26e77c837f6255cd630291bf
Brian Michalski <bmichalski@gmail.com>
2012-08-23 21:11:32 -0700

    Add FormattedText model.
    Scaffolded via: ./script/rails generate model FormattedText --old-style-hash --skip-migration --skip-namespace

---
+++ app/models/formatted_text.rb
@@ -0,0 +1,3 @@
+class FormattedText < ActiveRecord::Base
+  # attr_accessible :title, :body
+end
---
+++ test/unit/formatted_text_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FormattedTextTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

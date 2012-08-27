69800e00ebe30bad6c3e679cc21dec8a31c5c0a2
Brian Michalski <bmichalski@gmail.com>
2012-08-26 21:55:38 -0700

    Register the content type with the app.

--- lib/c2_html_text/engine.rb
+++ lib/c2_html_text/engine.rb
@@ -1,5 +1,9 @@
 module C2HtmlText
   class Engine < ::Rails::Engine
     isolate_namespace C2HtmlText
+
+    initializer "register content type" do |app|
+      app.config.content_types << FormattedText
+    end
   end
 end
